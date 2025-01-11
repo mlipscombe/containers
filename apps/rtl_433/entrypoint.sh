6b2ac46800541cb9a215233885148a1d11858a27
Mark Lipscombe <mark@lipscombe.com>
2025-01-11 14:18:28 -0500

    chore(misc): test a new build

---
+++ apps/rtl_433/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#shellcheck disable=SC2086
+
+/usr/local/bin/python \
+    "/app/rtl_433_mqtt_hass.py" \
+    ${RTL_433_MQTT_HASS_ARGS} &
+
+/usr/local/bin/rtl_433 \
+    ${RTL_433_ARGS} &
+
+# Wait for the process to finish
+wait $!
+
+exit $?

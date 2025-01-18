a1147ae61da52053747d7233d92bc0e00cb7ed9f
Mark Lipscombe <mark@lipscombe.com>
2025-01-18 10:23:41 -0500

    fix(wyoming-faster-whisper): disable tests

--- apps/wyoming-faster-whisper/entrypoint.sh
+++ apps/wyoming-faster-whisper/entrypoint.sh
@@ -4,8 +4,8 @@
 exec \
     /app/bin/python3 -m  wyoming_faster_whisper \
         --uri 'tcp://0.0.0.0:10300' \
-        --data-dir /data \
-        --download-dir /data \
+        --data-dir /config \
+        --download-dir /config \
         --model "${MODEL:-tiny-int8}" \
         --language "${LANGUAGE:-en}" \
         --device "${DEVICE:-cpu}" \

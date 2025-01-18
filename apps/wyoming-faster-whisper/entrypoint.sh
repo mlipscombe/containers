1587fe990757a7aa37859528de6431412366ca83
Mark Lipscombe <mark@lipscombe.com>
2025-01-18 09:47:45 -0500

    fix(wyoming-faster-whisper): try to fix build

--- apps/wyoming-faster-whisper/entrypoint.sh
+++ apps/wyoming-faster-whisper/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 #shellcheck disable=SC2086
 
-export HOME="${HOME:-/data}"
-export HF_HUB_CACHE="${HF_HUB_CACHE:-/data/.cache}"
-export TEMP="${TEMP:-${HOME}/.temp}"
-
 exec \
     /app/bin/python3 -m  wyoming_faster_whisper \
         --uri 'tcp://0.0.0.0:10300' \

0752d2a584392fa9ac99192dec818e55d88a1272
Mark Lipscombe <mark@lipscombe.com>
2025-04-05 12:12:00 -0400

    fix(exo): set variables

--- apps/exo/entrypoint.sh
+++ apps/exo/entrypoint.sh
@@ -2,5 +2,9 @@
 
 #shellcheck disable=SC2086
 exec \
-    /app/bin/exo --disable-tui --node-id $( [ -z "$NODE_ID" ] && NODE_ID=$(uuidgen); echo $NODE_ID ) \
+    /app/bin/exo --disable-tui --data "${EXO_DATA:-/data}" \
+        --listen-port "${EXO_LISTEN_PORT:-5000}" \
+        --node-port "${EXO_NODE_PORT:-5001}" \
+        --broadcast-port "${EXO_BROADCAST_PORT:-5002}" \
+        --node-id $( [ -z "$NODE_ID" ] && NODE_ID=$(uuidgen); echo $NODE_ID ) \
         "$@"

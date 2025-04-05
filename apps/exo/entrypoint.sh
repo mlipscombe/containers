3e0f24c4385d099f6071a43403183b9cdc66a6c9
Mark Lipscombe <mark@lipscombe.com>
2025-04-05 12:34:04 -0400

    feat(exo): set paths

--- apps/exo/entrypoint.sh
+++ apps/exo/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+EXO_DATA="${EXO_DATA:-/data}"
+NODE_ID=$( [ -z "$NODE_ID" ] && NODE_ID=$(uuidgen); echo $NODE_ID )
+HOME="${EXO_DATA}"
+TRANSFORMERS_CACHE="${EXO_DATA}/transformers_cache"
+mkdir -p "${TRANSFORMERS_CACHE}"
+
 #shellcheck disable=SC2086
 exec \
-    /app/bin/exo --disable-tui --data "${EXO_DATA:-/data}" \
+    /app/bin/exo --disable-tui --data "${EXO_DATA}" \
         --listen-port "${EXO_LISTEN_PORT:-5000}" \
         --node-port "${EXO_NODE_PORT:-5001}" \
         --broadcast-port "${EXO_BROADCAST_PORT:-5002}" \

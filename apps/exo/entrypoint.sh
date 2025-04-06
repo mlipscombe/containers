3cab47b7fcae43a36b1149bf3e63178c69422048
Mark Lipscombe <mark@lipscombe.com>
2025-04-06 12:41:22 -0400

    feat(exo): clean disk to avoid out of space

--- apps/exo/entrypoint.sh
+++ apps/exo/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -eo pipefail
 
 EXO_DATA="${EXO_DATA:-/data}"
 NODE_ID=$( [ -z "$NODE_ID" ] && NODE_ID=$(uuidgen); echo $NODE_ID )
@@ -8,9 +9,10 @@ mkdir -p "${TRANSFORMERS_CACHE}"
 
 #shellcheck disable=SC2086
 exec \
-    /app/bin/exo --disable-tui --data "${EXO_DATA}" \
+    /app/bin/python3 /app/bin/exo --disable-tui --data "${EXO_DATA}" \
         --listen-port "${EXO_LISTEN_PORT:-5000}" \
         --node-port "${EXO_NODE_PORT:-5001}" \
         --broadcast-port "${EXO_BROADCAST_PORT:-5002}" \
+        --chatgpt-api-port "${EXO_CHATGPT_API_PORT:-5003}" \
         --node-id $( [ -z "$NODE_ID" ] && NODE_ID=$(uuidgen); echo $NODE_ID ) \
         "$@"

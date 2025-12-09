d7ee2b356d6e80bc6def5ff7e9463b96a6c282ad
Mark Lipscombe <mark@lipscombe.com>
2025-12-09 06:19:26 -0500

    feat(canvas-lms): try running initial db and asset generation on startup instead of init container

--- apps/canvas-lms/entrypoint.sh
+++ apps/canvas-lms/entrypoint.sh
@@ -13,6 +13,9 @@ for file in /usr/src/nginx/{,**/}*.erb; do
   fi
 done
 
+bundle exec rake db:initial_setup
+bundle exec rake brand_configs:generate_and_upload_all
+
 ln -sf /proc/self/fd/1 /usr/src/app/log/production.log
 
 exec sudo -E /usr/sbin/nginx -c /usr/src/nginx/nginx.conf

465e033be73fefff1fa868d99697675b0a1190b4
Mark Lipscombe <mark@lipscombe.com>
2025-12-11 11:00:19 -0500

    feat(canvas-lms): fix run logic to run job server inside container

--- apps/canvas-lms/entrypoint.sh
+++ apps/canvas-lms/entrypoint.sh
@@ -1,41 +1,10 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -eo pipefail
 
-generate_temp_key() {
-    tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 32
-}
+echo "Running database setup..."
+rake db:initial_setup || true
 
-if [[ -z "${CANVAS_ENCRYPTION_KEY}" ]]; then
-    echo "WARNING: CANVAS_ENCRYPTION_KEY is not set. Using a temporary, randomly generated 32-character key. Do NOT use this in production."
-    CANVAS_ENCRYPTION_KEY="$(generate_temp_key)"
-    export CANVAS_ENCRYPTION_KEY
-fi
-
-if [[ -z "${CANVAS_JWT_ENCRYPTION_KEY}" ]]; then
-    echo "WARNING: CANVAS_JWT_ENCRYPTION_KEY is not set. Using a temporary, randomly generated 32-character key. Do NOT use this in production."
-    CANVAS_JWT_ENCRYPTION_KEY="$(generate_temp_key)"
-    export CANVAS_JWT_ENCRYPTION_KEY
-fi
-
-
-if [[ -z "${CANVAS_RCE_ENCRYPTION_SECRET}" ]]; then
-    echo "WARNING: CANVAS_RCE_ENCRYPTION_SECRET is not set. Using a temporary, randomly generated 32-character key. Do NOT use this in production."
-    CANVAS_RCE_ENCRYPTION_SECRET="$(generate_temp_key)"
-    export CANVAS_RCE_ENCRYPTION_SECRET
-fi
-
-
-if [[ -z "${CANVAS_RCE_SIGNING_SECRET}" ]]; then
-    echo "WARNING: CANVAS_RCE_SIGNING_SECRET is not set. Using a temporary, randomly generated 32-character key. Do NOT use this in production."
-    CANVAS_RCE_SIGNING_SECRET="$(generate_temp_key)"
-    export CANVAS_RCE_SIGNING_SECRET
-fi
-
-
-if [[ -z "${CANVAS_LTI_SIGNING_SECRET}" ]]; then
-    echo "WARNING: CANVAS_LTI_SIGNING_SECRET is not set. Using a temporary, randomly generated 32-character key. Do NOT use this in production."
-    CANVAS_LTI_SIGNING_SECRET="$(generate_temp_key)"
-    export CANVAS_LTI_SIGNING_SECRET
-fi
+echo "Generating brand configs..."
+rake brand_configs:generate_and_upload_all || true
 
 exec /sbin/tini -- "$@"
